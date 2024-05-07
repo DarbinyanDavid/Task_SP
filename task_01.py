e9964043e65e75d76516c74b916dbf15282343ed
Darbinyan David <kingdavit@19981886gmail.com>
2024-05-07 16:56:28 +0300

    Исправлено

--- task_01.py
+++ task_01.py
@@ -1,8 +1,26 @@
-def is_palindrome(data):
-    data = data.replace(' ', '').lower()
-    data = ''.join(filter(lambda c: c.isalpha(), data))
-    return True if data == data[::-1] else False
+def is_palindrome(input):
+    if isinstance(input, str):
+        cleaned_string = input.lower().replace(" ", "").replace(",", "").replace(".", "").replace("-", "").replace("!",
+                                                                                                                   "").replace(
+            "'", "")
+
+        return cleaned_string == cleaned_string[::-1]
+
+    elif isinstance(input, int):
+        string = str(input)
+
+        return string == string[::-1]
+
+    #
+    else:
+        return False
 
 
-print(is_palindrome("A man, a plan, a canal -- Panama"))  # => True
-
+print(is_palindrome("A man, a plan, a canal -- Panama"))  # True
+print(is_palindrome("Madam, I'm Adam!"))  # True
+print(is_palindrome(333))  # True
+print(is_palindrome(None))  # False
+print(is_palindrome("Abracadabra"))  # False
+print(is_palindrome(12321))  # True
+print(is_palindrome(12345))  # False
+print(is_palindrome(100001))  # True
