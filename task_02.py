5099a8c0969c7ba19527dad90f892c1a9c1acfe8
Darbinyan David <kingdavit@19981886gmail.com>
2024-05-08 14:23:42 +0300

    Исправлено

--- task_02.py
+++ task_02.py
@@ -1,8 +1,6 @@
-def coincidence(lst, rng):
-    if not isinstance(lst, list):
-        raise TypeError("lst должен быть списком.")
-    if not isinstance(rng, range):
-        raise TypeError("rng должен быть диапазоном.")
+def coincidence(lst=[], rng=range(0)):
+    if not lst or not rng:
+        return []
 
     result = []
 
@@ -25,7 +23,7 @@ def coincidence(lst, rng):
     return sorted(result)
 
 
-# Протестируем функцию с заданным тестовым случаем
+# Тесты
+print(coincidence([1, 2, 3, 4, 5], range(3, 6)))  # => [3, 4, 5]
+print(coincidence())  # => []
 print(coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4)))  # => [1, 2, 2.5]
-
-
