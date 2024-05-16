248f257a17ce51a7f87302a4e897950374139087
Darbinyan David <kingdavit@19981886gmail.com>
2024-05-16 18:17:39 +0300

    Исправлено

--- task_11.py
+++ task_11.py
@@ -17,7 +17,13 @@ class Dessert:
 
     @calories.setter
     def calories(self, value):
-        self._calories = value
+        if isinstance(value, int):
+            self._calories = value
+        else:
+            try:
+                self._calories = int(value)
+            except ValueError:
+                raise ValueError("Calories must be an integer")
 
     def is_healthy(self) -> bool:
         return self.calories < 200
@@ -30,7 +36,29 @@ class Dessert:
 # Пример использования
 dessert = Dessert()
 
-print(dessert.name)  # Cake
-print(dessert.calories)  # 300
-print(dessert.is_healthy())  # False
-print(Dessert.is_delicious())  # True
+print(dessert.name)  # Пустая строка, так как значение по умолчанию
+print(dessert.calories)  # 0, так как значение по умолчанию
+print(dessert.is_healthy())  # True, так как калорийность меньше 200
+print(Dessert.is_delicious())  # True, так как любой десерт считается вкусным
+
+# Тестовые команды
+dessert = Dessert()
+dessert.name = "test_name"
+print(dessert.name)
+dessert.name = "test_name2"
+print(dessert.name)
+if dessert.name != "test_name2":
+    raise Exception("Setter for name is not working")
+
+dessert.calories = 150
+print(dessert.calories)
+dessert.calories = 250
+print(dessert.calories)
+if dessert.calories != 250:
+    raise Exception("Setter for calories is not working")
+
+print(dessert.is_delicious())
+if not dessert.is_delicious():
+    raise Exception("Invalid method result")
+
+print(dessert.is_healthy())
